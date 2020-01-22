41965ee10b08e77bdb004c683fe44186a129778b
John Murray <lucidbard@users.noreply.github.com>
2020-01-22 09:46:39 -0500

    Setup for In-Class Exercise

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,23 +1,16 @@
 console.log("Hello World");
-var x = 1;
-// var y = new Array();
-// y[0] = 1;
-
-var y = [1];
-function scopeDemo(y1) {
+var x = [1];
+function scopeDemo(x1) {
+    console.log(x1);
     console.log(x);
     let a = 3, b = 4, c = 5;
+    x1[0] = 500;
     console.log(a);
     console.log(b);
     console.log(c);
-    console.log(y1);
-    y1[0] = 500;
-    var x = 2;
     if(true) {
         var x = 0;
     }
-    return a;
 }
-x = scopeDemo(y1);
-console.log(y);
-console.log(0);
+scopeDemo(x);
+console.log(x);
