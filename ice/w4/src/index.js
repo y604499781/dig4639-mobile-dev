5d8fe604b09a7977b9cf21310e957ee9c41efca7
John Murray <jo284142@ucf.edu>
2020-01-29 10:30:10 -0500

    In class exercise starter code

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -1,15 +1,15 @@
 import Component from "./Component.js";
+import Task from "./Task.js";
 function runOnLoad()
 {
     
     let element = document.createElement("div");
-    element.id = "container";
+    element.id = "Test";
     document.body.appendChild(element);
     // console.log(x);
     var comp = new Component(document.getElementById("addTask"));
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
-    container.appendChild(element2);
     console.log("Hello World truly");
 }
 
