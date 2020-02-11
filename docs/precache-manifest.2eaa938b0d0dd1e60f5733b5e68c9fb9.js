6f891273215063c129ed0f0fb8bda3dea399f3e7
John Murray <lucidbard@users.noreply.github.com>
2020-02-11 11:39:59 -0500

    Added github pages content

---
+++ docs/precache-manifest.2eaa938b0d0dd1e60f5733b5e68c9fb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e26c2c61ffc581fa8bd78229ee6298d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a47d9042d396eefe9ba2",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47d9042d396eefe9ba2",
+    "url": "/static/js/main.1e34b9d4.chunk.js"
+  },
+  {
+    "revision": "ba5262a905c6bbcf362f",
+    "url": "/static/js/runtime-main.02da7130.js"
+  }
+]);
