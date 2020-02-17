b09e84dd73f19a1601e6a93cb4e7bf1317d827be
John Murray <lucidbard@users.noreply.github.com>
2020-02-17 08:38:39 -0500

    Week 7 Starter Code

---
+++ ice/w7a/MyMath.test.js
@@ -0,0 +1,27 @@
+import { Sum } from './sum.js'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(Sum(1, 2)).toBe(3)
+})
+
+// ICE 
+
+// Examples for Null
+test('null', () => {
+  const n = null
+  expect(n).toBeNull()
+  expect(n).toBeDefined()
+  expect(n).not.toBeUndefined()
+  expect(n).not.toBeTruthy()
+  expect(n).toBeFalsy()
+})
+
+// Examples for zero
+test('zero', () => {
+  const z = 0
+  expect(z).not.toBeNull()
+  expect(z).toBeDefined()
+  expect(z).not.toBeUndefined()
+  expect(z).not.toBeTruthy()
+  expect(z).toBeFalsy()
+})
