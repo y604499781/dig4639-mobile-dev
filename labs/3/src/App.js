95f012a284a0ee07d96a5fdfd400f93c20445047
Yi Chen <yi717123@ucf.edu>
2020-02-21 13:23:08 -0500

    init commit

---
+++ labs/3/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card/index.js';
+
+/*class App extends React.Component {
+  render() {
+    return (
+      <Card content="This is a Card!"></Card>
+    );
+  }
+}*/
+const App =() => {
+  return (
+    <Card content="This is a card!"></Card>
+  );
+}
+
+export default App;
---
+++ labs/3/src/components/Card/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./index.css"
+
+class Card extends React.Component {    
+    render() {
+        return (
+            <div className="card">rhwr</div>
+        );
+    }
+}
+export default Card;
